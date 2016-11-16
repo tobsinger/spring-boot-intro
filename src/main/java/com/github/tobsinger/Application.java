7316bf8943196599d5ebb6386587c7b755128ed2
Tobias Weidinger <tobias.weidinger@gmx.de>
2016-11-16 13:06:30 +0100

    exclude default error endpoints from swagger documentation

--- src/main/java/com/github/tobsinger/Application.java
+++ src/main/java/com/github/tobsinger/Application.java
@@ -2,13 +2,20 @@ package com.github.tobsinger;
 
 import com.github.tobsinger.entity.Customer;
 import com.github.tobsinger.repository.CustomerRepository;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 @SpringBootApplication
@@ -32,4 +39,12 @@ public class Application {
         };
     }
 
+    @Bean
+    public Docket demoApi() {
+        return new Docket(DocumentationType.SWAGGER_2)//<3>
+                .select()//<4>
+                .apis(RequestHandlerSelectors.any())//<5>
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))//<6>
+                .build();
+    }
 }
